2c310158606d5f619fcb83c204382301bae86d5b
bhargav yogesh <bhargav.yogesh@gmail.com>
2020-07-03 21:28:17 -0400

    fixed api key issues

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function setPlugWeather(){
     const plug = client.getDevice({host: '192.168.0.143'}).then((device)=>{
         //device.getSysInfo().then(console.log)
         hour = new Date().getHours();
-        axios.get('https://api.darksky.net/forecast/80e2bdad12ff5f90a5f6b6762c90748a/' + process.env.LATITUDE + ',' + process.env.LONGITUDE).then((response) => {
+        axios.get('https://api.darksky.net/forecast/' + process.env.DARKSKY + '/' + process.env.LATITUDE + ',' + process.env.LONGITUDE).then((response) => {
             temp = response.data.currently.temperature
             console.log(temp)
             device.setPowerState(temp > 74 || hour > 20 || hour < 8);
