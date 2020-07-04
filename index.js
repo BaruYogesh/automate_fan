da17794a8842bc84af00cc74ff5c34ebcca4a26b
Baru Yogesh <bhargav.yogesh@gmail.com>
2020-07-03 21:30:33 -0400

    changes to temp

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const client = new Client();
 
 
 function setPlugWeather(){
-    const plug = client.getDevice({host: '192.168.0.143'}).then((device)=>{
+    const plug = client.getDevice({host: process.env.PLUG_IP}).then((device)=>{
         //device.getSysInfo().then(console.log)
         hour = new Date().getHours();
         axios.get('https://api.darksky.net/forecast/' + process.env.DARKSKY + '/' + process.env.LATITUDE + ',' + process.env.LONGITUDE).then((response) => {
             temp = response.data.currently.temperature
             console.log(temp)
-            device.setPowerState(temp > 74 || hour > 20 || hour < 8);
+            device.setPowerState(temp > 80 || hour > 20 || hour < 8);
         })
         
     });
